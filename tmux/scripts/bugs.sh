b9aa9ddd0cc356b86a41b444a15c1e3bec62be74
HassanIrfan527 <harryed525@gmail.com>
2026-04-20 20:47:53 +0500

    add tmux script to show the bugs i have captured so far in the bottom powerline

---
+++ tmux/scripts/bugs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+c=$(find '/mnt/PERSONAL/Obsidian/SE 101/Bugs' -maxdepth 1 -type f -name '*.md' ! -name 'README.md' 2>/dev/null | wc -l)
+[ "$c" -gt 0 ] && printf $'#[fg=#303030,bg=default]\xee\x82\xb6#[fg=#f9e2af,bg=#303030] \xef\x86\x88  %s #[fg=#303030,bg=default]\xee\x82\xb4 ' "$c"
