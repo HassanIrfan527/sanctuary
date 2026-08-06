9517f1f314fb44fb0c103c4ceed50ffbb057b412
HassanIrfan527 <harryed525@gmail.com>
2026-08-07 03:05:27 +0500

    remove tmux configuration

--- tmux/scripts/bugs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-c=$(timeout 0.3 find '/mnt/PERSONAL/Obsidian/Personal/05-Work/Bugs' -maxdepth 1 -type f -name '*.md' ! -name 'README.md' 2>/dev/null | wc -l)
-[ "$c" -gt 0 ] && printf $'#[fg=#303030,bg=default]\xee\x82\xb6#[fg=#f9e2af,bg=#303030] \xef\x86\x88  %s #[fg=#303030,bg=default]\xee\x82\xb4 ' "$c"
