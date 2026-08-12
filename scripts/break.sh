48c27595a4584ba38d2ee3c02418fad4e5b6f139
HassanIrfan527 <harryed525@gmail.com>
2026-08-12 16:50:36 +0500

    remove unnecessary files (waybar and scripts)

--- scripts/break.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Send a desktop notification with a critical urgency so it stays on screen
-notify-send -u critical "✨ Night Shift Break" "Time to step away from the screen and meditate for 10 minutes."
